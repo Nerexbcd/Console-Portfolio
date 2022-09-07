f31fca336b348fc6e5bac3a998c79991d5a3684e
APáscoa <abiliodpascoa@gmail.com>
2022-09-07 15:00:47 +0100

    Improvements

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var socials = ["Github"]
+var socialsref = ["https://github.com/apascoa"]
+var aboutmetxt = "Under development"
+
 function newmainp(){
     var p = document.createElement('p');
     p.innerHTML = "User@Portfolio:~ $";
@@ -9,12 +13,34 @@ function newmainp(){
     document.getElementById("EndOfPage").scrollIntoView();
 }
 function newmenu(){
+    var title = document.createElement('p');
+    title.innerHTML = "### Menu ###";
+    title.className = "yellow";
+    document.getElementById('textcont').appendChild(title);
     var homeopt = document.createElement('p');
+    var aboutopt = document.createElement('p');
+    var socialopt = document.createElement('p');
+    var clearopt = document.createElement('p');
     homeopt.innerHTML = "Home";
+    aboutopt.innerHTML = "About";
+    socialopt.innerHTML = "Social";
+    clearopt.innerHTML = "Clear";
     homeopt.className = "orange";
+    aboutopt.className = "orange";
+    socialopt.className = "orange";
+    clearopt.className = "orange";
     homeopt.id = "selecting";
+    aboutopt.id = "selecting";
+    socialopt.id = "selecting";
+    clearopt.id = "selecting";
     homeopt.onclick = function(){home();optselected();};
-    document.getElementById('textcont').appendChild(homeopt)
+    aboutopt.onclick = function(){about();optselected();};
+    socialopt.onclick = function(){social();optselected();};
+    clearopt.onclick = function(){clear();};
+    document.getElementById('textcont').appendChild(homeopt);
+    document.getElementById('textcont').appendChild(aboutopt);
+    document.getElementById('textcont').appendChild(socialopt);
+    document.getElementById('textcont').appendChild(clearopt);
     newmainp();
 }
 function menuopts(){
@@ -23,7 +49,7 @@ function menuopts(){
     menuopt.className = "orange";
     menuopt.id = "selecting";
     menuopt.onclick = function(){menu();};
-    document.getElementById('textcont').appendChild(menuopt)
+    document.getElementById('textcont').appendChild(menuopt);
 }
 function menu () {
     document.getElementById("selecting").onclick = function(){};
@@ -36,9 +62,11 @@ function menu () {
 }
 function optselected(){
     newmainp();
-    document.getElementById("selecting").onclick = function(){};
-    document.getElementById("selecting").className = "blue";
-    document.getElementById("selecting").id = "finished";
+    for (var i = 0; i < 4 ; i++) {
+        document.getElementById("selecting").onclick = function(){};
+        document.getElementById("selecting").className = "blue";
+        document.getElementById("selecting").id = "finished";
+    }
 }
 function start(){
     document.getElementById("text").className = "";
@@ -51,13 +79,51 @@ function home(){
     document.getElementById("text").className = "";
     document.getElementById("text").innerHTML = "Home";
     document.getElementById("text").id = "finished";
+    var warning = document.createElement('p');
     var title = document.createElement('p');
     title.innerHTML = "### Welcome to my Portfolio ###";
+    warning.innerHTML = "!! Website in Development !!";
+    title.className = "yellow";
+    warning.className = "red";
+    document.getElementById('textcont').appendChild(title);
+    document.getElementById('textcont').appendChild(warning);
+    menuopts();
+}
+function about(){
+    document.getElementById("text").className = "";
+    document.getElementById("text").innerHTML = "About";
+    document.getElementById("text").id = "finished";
+    var aboutme = document.createElement('p');
+    var title = document.createElement('p');
+    title.innerHTML = "### About Me ###";
+    title.className = "yellow";
+    aboutme.innerHTML = aboutmetxt;
+    document.getElementById('textcont').appendChild(title);
+    document.getElementById('textcont').appendChild(aboutme);
+    menuopts();
+}
+function social(){
+    document.getElementById("text").className = "";
+    document.getElementById("text").innerHTML = "Social";
+    document.getElementById("text").id = "finished";
+    var title = document.createElement('p');
+    title.innerHTML = "### Social ###";
     title.className = "yellow";
     document.getElementById('textcont').appendChild(title);
-    var p = document.createElement('p');
-    p.innerHTML = "Website in Development";
-    p.className = "red";
-    document.getElementById('textcont').appendChild(p);
+    for (var i = 0; i < socials.length; i++) {
+        var social = document.createElement('a');
+        social.innerHTML = socials[i];
+        social.className = "orange";
+        social.href = socialsref[i];
+        social.target = "_blank";
+        document.getElementById('textcont').appendChild(document.createElement('p')).appendChild(social);
+    }
     menuopts();
+}
+function clear(){
+    document.getElementById("text").className = "";
+    document.getElementById("text").innerHTML = "Clear";
+    document.getElementById("text").id = "finished";
+    document.getElementById("textcont").innerHTML = "";
+    newmenu();
 }
